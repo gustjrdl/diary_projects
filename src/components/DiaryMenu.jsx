fdadec7f5dba6bb8c708b7f6332ccf4131dc0109
allwing12 <allwing1232@gmail.com>
2022-08-12 18:43:05 +0900

    아무거나

--- src/components/DiaryMenu.jsx
+++ src/components/DiaryMenu.jsx
@@ -4,6 +4,7 @@ function DiaryMenu() {
   return (
     <div className="DiaryMenu">
       <button>일기 보기</button>
+      <br />
       <button>일기 쓰기</button>
       <button>일기 수정하기</button>
       <button>일기 삭제하기</button>
